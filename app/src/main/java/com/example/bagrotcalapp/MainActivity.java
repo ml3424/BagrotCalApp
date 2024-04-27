1fc7cfe155f48a5ae96cb5421e595ae051704d81
ml3424 <ml3424@bs.amalnet.k12.il>
2024-04-27 16:14:48 +0300

    screen 2 base

--- app/src/main/java/com/example/bagrotcalapp/MainActivity.java
+++ app/src/main/java/com/example/bagrotcalapp/MainActivity.java
@@ -4,22 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Toast toast;
+    Button nextBtn;
+    EditText eTname, eTlanguage, eTliterature, eThistory, eTnationality, eTbible;
+
+    String nameG;
+    int languageG, literatureG, historyG, nationalityG, bibleG;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eTname = findViewById(R.id.eTname);
+        eTlanguage = findViewById(R.id.eTlanguage);
+        eTliterature = findViewById(R.id.eTliterature);
+        eThistory = findViewById(R.id.eThistory);
+        eTnationality = findViewById(R.id.eTnationality);
+        eTbible = findViewById(R.id.eTbible);
     }
 
-    boolean checkInput(String input)
+    boolean checkInputGrade(int input)
     {
-        if(Integer.valueOf(input) > 101)
+        if(input > 101)
         {
             toast = Toast.makeText(getApplicationContext(),"grade is a number between 1-100. try again",Toast.LENGTH_SHORT);
             return false;
@@ -28,5 +43,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nextScreen(View view) {
+        // get valid grades from the editexts and the user name.
+        nameG = eTname.getText().toString();
+
+        do {
+            languageG = Integer.valueOf(eTlanguage.getText().toString());
+        } while(checkInputGrade(languageG));
+        do {
+            literatureG = Integer.valueOf(eTliterature.getText().toString());
+        } while(checkInputGrade(literatureG));
+        do {
+            historyG = Integer.valueOf(eThistory.getText().toString());
+        } while(checkInputGrade(historyG));
+        do {
+            nationalityG = Integer.valueOf(eTnationality.getText().toString());
+        } while(checkInputGrade(nationalityG));
+        do {
+            bibleG = Integer.valueOf(eTbible.getText().toString());
+        } while(checkInputGrade(bibleG));
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/bagrotcalapp/MogbarSubjects.java
@@ -0,0 +1,14 @@
+package com.example.bagrotcalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MogbarSubjects extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mogbar_subjects);
+    }
+}
